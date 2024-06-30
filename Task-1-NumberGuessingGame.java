9f224a365fdac1b83e50771cfe39353c1a64dade
Ravi Kumar <148791678+RaviKumar25466@users.noreply.github.com>
2024-06-30 13:04:43 +0530

    Add files via upload

---
+++ Task-1-NumberGuessingGame.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class NumberGuessingGame {
+	public static void main(String[] args) {
+		        int randomNumber = (int) (Math.random() * 100) + 1;
+		        Scanner scanner = new Scanner(System.in);
+		        boolean guessedCorrectly = false;
+		        int numberOfAttempts = 0;
+		        System.out.println("Guess a number between 1 and 100: ");
+
+		        while (!guessedCorrectly && numberOfAttempts < 7) 
+		        {
+		            int userGuess = scanner.nextInt();
+		            if (userGuess == randomNumber) 
+		            {
+		                guessedCorrectly = true;
+		            } 
+		            else 
+		            {
+		                numberOfAttempts++;
+		                if (userGuess > randomNumber) 
+		                {
+		                    System.out.println("Your guess is too high.");
+		                } else 
+		                {
+		                    System.out.println("Your guess is too low.");
+		                }
+		            }
+		        }
+
+		        scanner.close();
+		        if (guessedCorrectly) 
+		        {
+		            System.out.println("Congratulations! You guessed correctly! The number was " + randomNumber);
+		        } else 
+		        {
+		            System.out.println("Sorry, you ran out of guesses. The correct answer was " + randomNumber);
+		        }
+	}
+
+}
